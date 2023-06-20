b86dc3156df42272d2028c94fa527ab7aecbf6c0
Mike <lumen.net@yandex.ru>
2023-06-20 17:28:48 +0400

    Added inline search with delay

--- src/components/book-search/BookSearch.vue
+++ src/components/book-search/BookSearch.vue
@@ -1,26 +1,27 @@
 <script setup lang="ts">
 import { getBooks } from '@/api/books-service';
+import { debounce } from '@/utils/debounce';
 import BookSearchList from './BookSearchList.vue'; 
 </script> 
 
 
 <template>
   <div class="toolbar">
-    <form @submit.prevent="search">
-      <div>
-        <input type="text" v-model="keyword" placeholder="Search..." class="input" required>
-        <!-- <input type="text" v-model="query" placeholder="Search..." class="input" required> -->
-        <!-- <div>Value: {{ query }}</div> -->
-        <input type="submit" value="Search" class="button">
-      </div>
-      <div>
-        <label for="order">Order by</label>&nbsp;
-        <select name="order" v-model="orderBy" @change="search">
-          <option value="newest">newest</option>
-          <option value="relevance">relevance</option>
-        </select>
-      </div>
-    </form>
+    <!-- <form @submit.prevent="search"> -->
+    <div>
+      <!-- <input type="text" v-model="keyword" placeholder="Search..." class="input" required> -->
+      <input type="text" v-model="keyword" placeholder="Search..." class="input" required>
+      <div>Value: {{ debouncedKeyword }}</div>
+      <!-- <input type="submit" value="Search" class="button"> -->
+    </div>
+    <div>
+      <label for="order">Order by</label>&nbsp;
+      <select name="order" v-model="orderBy" @change="search">
+        <option value="newest">newest</option>
+        <option value="relevance">relevance</option>
+      </select>
+    </div>
+    <!-- </form> -->
   </div>
 
   <BookSearchList :books=books />
@@ -34,21 +35,28 @@ export default {
     return {
       books: [] as any,
       keyword: '',
+      debouncedKeyword: '',
       orderBy: 'newest',
       maxResults: '10',
       loadState: ''
     }
   },
+  watch: {
+    keyword: debounce(function (newVal) {
+      console.log(newVal)
+    }, 500)
+  },
   methods: {
     search() {
       this.loadState = 'loading'
-      getBooks(this.keyword)
-        .then((response) => {
-          this.books = response
-          this.loadState = 'success'
-          console.log(response)
-        })
-        .catch((error) => console.log(error))
+      console.log(123)
+      // getBooks(this.keyword)
+      //   .then((response) => {
+      //     this.books = response
+      //     this.loadState = 'success'
+      //     console.log(response)
+      //   })
+      //   .catch((error) => console.log(error))
     }
   }
 }
