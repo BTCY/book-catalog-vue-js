1a823a1869154143a2a54e86b01a11e4971f7555
Mike <lumen.net@yandex.ru>
2023-06-21 07:49:19 +0400

    Improved book search. Connected the call delay, removed the extra layout.

--- src/components/book-search/BookSearch.vue
+++ src/components/book-search/BookSearch.vue
@@ -7,12 +7,8 @@ import BookSearchList from './BookSearchList.vue';
 
 <template>
   <div class="toolbar">
-    <!-- <form @submit.prevent="search"> -->
     <div>
-      <!-- <input type="text" v-model="keyword" placeholder="Search..." class="input" required> -->
       <input type="text" v-model="keyword" placeholder="Search..." class="input" required>
-      <div>Value: {{ debouncedKeyword }}</div>
-      <!-- <input type="submit" value="Search" class="button"> -->
     </div>
     <div>
       <label for="order">Order by</label>&nbsp;
@@ -21,7 +17,6 @@ import BookSearchList from './BookSearchList.vue';
         <option value="relevance">relevance</option>
       </select>
     </div>
-    <!-- </form> -->
   </div>
 
   <BookSearchList :books=books />
@@ -35,28 +30,26 @@ export default {
     return {
       books: [] as any,
       keyword: '',
-      debouncedKeyword: '',
       orderBy: 'newest',
       maxResults: '10',
       loadState: ''
     }
   },
   watch: {
-    keyword: debounce(function (newVal) {
-      console.log(newVal)
+    keyword: debounce(function () {
+      this.search()
     }, 500)
   },
   methods: {
     search() {
       this.loadState = 'loading'
-      console.log(123)
-      // getBooks(this.keyword)
-      //   .then((response) => {
-      //     this.books = response
-      //     this.loadState = 'success'
-      //     console.log(response)
-      //   })
-      //   .catch((error) => console.log(error))
+      getBooks(this.keyword)
+        .then((response) => {
+          this.books = response
+          this.loadState = 'success'
+          console.log(response)
+        })
+        .catch((error) => console.log(error))
     }
   }
 }
--- src/utils/debounce.tsx
+++ src/utils/debounce.tsx
@@ -1,10 +1,20 @@
-export function debounce<T extends (...args: any[]) => void>(fn: T, delay: number): T {
+/**
+ *  Delayed function call.
+ * 
+ * @param   {Function}   fn       Function to be called. 
+ * @param   {number}     delayMS  After how long it will be called (in milliseconds). 
+ * @return  {T}                   Returns a function to be executed with a delay.
+ */
+export function debounce<T extends (...args: any[]) => void>(
+    fn: T,
+    delayMS: number
+): T {
     let timeoutID: number | undefined = undefined;
 
     return function (this: any, ...args: any[]) {
         clearTimeout(timeoutID);
         timeoutID = setTimeout(() => {
             fn.apply(this, args);
-        }, delay);
+        }, delayMS);
     } as T;
 }
