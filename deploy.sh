6fbea6ecf38ba4efad8de11584781cea080f3245
Mike <lumen.net@yandex.ru>
2023-07-04 15:36:16 +0400

    Gh-pages settings

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+ 
+set -e
+ 
+npm run build
+ 
+cd dist
+ 
+
+git init
+git add -A
+git commit -m 'deploy'
+ 
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:BTCY/book-catalog-vue-js.git master:gh-pages
+
+cd -
