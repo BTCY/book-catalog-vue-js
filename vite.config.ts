b68c8a0fa8f7d36a8ef578b2f2f67f63c4abb90d
Mike <lumen.net@yandex.ru>
2023-07-04 16:34:03 +0400

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/book-catalog-vue-js/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
